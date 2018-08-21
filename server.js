12d0bbffadfbf728f984377e39fb6ee49e5d5921
morishima311 <ryoryo311@gmail.com>
2018-08-21 12:21:22 +0900

    add index.ejs to see a text on a browser

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
-
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate')
+var session = require('express-session');
 
 var app = express();
 
+app.use(express.static('public'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.listen(3000, function(){console.log('Listening on port 3000')})
+app.get('/', function(req, res, next){
+  res.render('index');
+});
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000');
+});
