71923382e0bc0012ec1be14032e7c0e61dfd0b6f
morishima311 <ryoryo311@gmail.com>
2018-08-22 10:05:12 +0900

    add express-session and mongoose

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var engine = require('ejs-mate')
 var session = require('express-session');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
+
+mongoose.connect('monogodb://localhost/rateme');
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
@@ -13,6 +17,13 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'Thisismytestkey',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 app.get('/', function(req, res, next){
   res.render('index');
